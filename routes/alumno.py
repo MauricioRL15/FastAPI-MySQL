c23efa0e796169e62129ea35bd1b41bfe13817f9
MauricioRL15 <15rlmauricio@gmail.com>
2023-07-04 16:24:11 -0600

    Volver a hacer esto muchas veces

--- routes/alumno.py
+++ routes/alumno.py
@@ -10,7 +10,6 @@ def getAll():
     return alumnos
 
 #Servicio para insertar un alumno
-
 @router.post('/insert')
 def insert(alumno:Alumno):
     alumnos.append(dict(alumno))
@@ -18,3 +17,9 @@ def insert(alumno:Alumno):
         "status":"Alumno insertado"
     }
     return response
+
+#Servicio para consultar alumnos por matricula
+@router.get('/getOne/{matricula}')
+def getOne(matricula):
+    #Aqui va el codigo para buscar un alumno en la lista
+    return "Hola"
